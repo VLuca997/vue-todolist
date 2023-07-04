132beb6b630b4d8d611060c660d19a0c55b4e88f
VLuca997 <kulertoast997@gmail.com>
2023-07-05 01:09:43 +0200

    Milestone 1 + Milestone 2

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//CREATE APP VUE
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data(){
+        return{
+            toDoList: [
+                {   
+                    text: `Test1`,
+                    done: false,
+                },
+                {
+                    text:`Test2`,
+                    done: true,
+                },
+            ],
+            newToDo: ``,
+        };
+    },
+    methods: {
+        addToDo() {
+            if(this.newToDo.trim() !== ``){// SE newToDo non è vuoto ( e con .trim anche se non è composto SOLO da spazi bianchi)
+                this.toDoList.push({ // ALLORA aggiungo un nuovo elemento all' ARRAY toDoList
+                    text :this.newToDo,
+                    done:false,
+                });
+            }
+            // ALTRIMENTI SVUOTA L'INPUT
+                this.newToDo = ``;
+            
+
+        },
+        removeTodo(index) {
+            this.toDoList.splice(index, 1);
+        },
+        toggleTodoDone(todo) {
+            todo.done = !todo.done;
+        },
+    }
+});
+app.mount(`#app`);
